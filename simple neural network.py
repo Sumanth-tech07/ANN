1178a093ed34642ed8ab5b30256cc5e563cea0d9
Sumanth Rachakonda <sumanthrachakonda4@gmail.com>
2026-02-04 16:29:26 +0530

    simple neural network.py

---
+++ simple neural network.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+model=nn.Sequential(
+nn.Linear(2,4),
+nn.ReLU(),
+nn.Linear(4,1),
+)
+x=torch.tensor([[0.,0.],[0.,1.],[1.,0.],[1.,1.]]) # input features
+y=torch.tensor([[0.],[1.],[1.],[0.]]) # output features
+loss_fn = nn.MSELoss()
+opt=torch.optim.Adam(model.parameters(),lr=0.01)
+for _ in range(1000):
+    opt.zero_grad()
+    loss=loss_fn(model(x),y)
+    loss.backward()
+    opt.step()
+print(model(x))
+
